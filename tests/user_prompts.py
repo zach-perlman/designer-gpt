bf0db1f6e8f65d27a0615219244615a34130e694
OfficialYrahcaz <zachperlman20@gmail.com>
2023-12-03 22:49:31 -0500

    Test prompts added

---
+++ tests/user_prompts.py
@@ -0,0 +1,39 @@
+# Test User Prompts
+
+PROMPT_FASHION = """\
+Create a website design for a small business specializing in handmade crafts, with an emphasis on showcasing individual products and enabling easy online purchases.
+""".strip()
+
+PROMPT_FITNESS = """\
+Design a dynamic website for a fitness center, including sections for class schedules, trainer profiles, membership plans, and a blog on health and wellness.
+""".strip()
+
+PROMPT_TRAVEL = """\
+Create a captivating design for a travel blog, with space for blog posts, travel tips, photo galleries, and a section for reader interactions.
+""".strip()
+
+PROMPT_BAKERY = """\
+Create a warm, inviting website for a local bakery, featuring a menu, special offers, customer testimonials, and an online order form.
+""".strip()
+
+PROMPT_NONPROFIT = """\
+Design a website for a non-profit organization focused on environmental conservation, highlighting their mission, ongoing projects, and ways to donate or volunteer.
+""".strip()
+
+# The following are more specific
+
+PROMPT_BOOKSTORE = """\
+Create a website for an online bookstore, including a homepage with featured books, a detailed search function with filters (genre, author, price), individual book pages with reviews and ratings, a shopping cart system, and a user account area.
+""".strip()
+
+PROMPT_UNIVERSITY = """\
+Design a structured website for a university website, featuring a homepage with departmental navigation, individual pages for each department with course listings, a faculty directory, a student portal with login, and an events calendar section.
+""".strip()
+
+PROMPT_VIRTUAL_MUSEUM = """\
+Develop a website for an interactive virtual museum focusing on lost civilizations, featuring a 3D interactive map for virtual tours, detailed exhibits with multimedia content, a timeline of civilizations, a guest curator section, and educational resources for schools.
+""".strip()
+
+PROMPT_SPACE_GAME = """\
+Craft a website for a website dedicated to a space exploration simulation game, including a homepage with game trailers, a section for game guides and tutorials, player leaderboards, an online store for game merchandise, and a community forum for player collaborations.
+""".strip()
