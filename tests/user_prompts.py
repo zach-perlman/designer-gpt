0b58577393515fc03e036ef464a5fcc6a0459c30
OfficialYrahcaz <zachperlman20@gmail.com>
2023-12-04 01:44:25 -0500

    Added test prompts

--- tests/user_prompts.py
+++ tests/user_prompts.py
@@ -19,7 +19,7 @@ Design a website for a non-profit organization focused on environmental conserva
 # The following are more specific
 
 PROMPT_BOOKSTORE = """\
-Create a website for an online bookstore, including a homepage with featured books, a detailed search function with filters (genre, author, price), individual book pages with reviews and ratings, a shopping cart system, and a user account area.
+Design a website for an online bookstore, including a homepage with featured books, a detailed search function with filters (genre, author, price), individual book pages with reviews and ratings, a shopping cart system, and a user account area.
 """.strip()
 
 PROMPT_UNIVERSITY = """\
@@ -27,9 +27,17 @@ Design a structured website for a university website, featuring a homepage with 
 """.strip()
 
 PROMPT_VIRTUAL_MUSEUM = """\
-Develop a website for an interactive virtual museum focusing on lost civilizations, featuring a 3D interactive map for virtual tours, detailed exhibits with multimedia content, a timeline of civilizations, a guest curator section, and educational resources for schools.
+Design a website for an interactive virtual museum focusing on lost civilizations, featuring a 3D interactive map for virtual tours, detailed exhibits with multimedia content, a timeline of civilizations, a guest curator section, and educational resources for schools.
 """.strip()
 
 PROMPT_SPACE_GAME = """\
-Craft a website for a website dedicated to a space exploration simulation game, including a homepage with game trailers, a section for game guides and tutorials, player leaderboards, an online store for game merchandise, and a community forum for player collaborations.
+Design a website for a website dedicated to a space exploration simulation game, including a homepage with game trailers, a section for game guides and tutorials, player leaderboards, an online store for game merchandise, and a community forum for player collaborations.
+""".strip()
+
+PROMPT_AI = """\
+Craft a wireframe for a website focused on the ethics of artificial intelligence, including a homepage with recent AI news, a discussion forum for ethical debates, a resource library with research papers and articles, profiles of leading AI ethicists, and a section for online workshops and seminars.
+""".strip()
+
+PROMPT_MUSIC = """\
+Design a wireframe for an underground music scene platform, featuring a dynamic homepage with featured artists, a music player with curated playlists, a blog with artist interviews and reviews, a calendar of underground events and gigs, and a section for user-generated content.
 """.strip()
